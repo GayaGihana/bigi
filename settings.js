9e52c12e76e97ca29ef944f46fbd14708b84f178
GayaGihana <159061495+GayaGihana@users.noreply.github.com>
2024-04-25 17:43:17 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,89 @@
+/*
+
+
+███████╗██╗░░░██╗░██████╗██╗░░░██╗░█████╗░░█████╗░  ░░░░░░  ███╗░░░███╗██████╗░
+╚════██║██║░░░██║██╔════╝╚██╗░██╔╝██╔══██╗██╔══██╗  ░░░░░░  ████╗░████║██╔══██╗
+░░███╔═╝██║░░░██║╚█████╗░░╚████╔╝░██║░░╚═╝██║░░██║  █████╗  ██╔████╔██║██║░░██║
+██╔══╝░░██║░░░██║░╚═══██╗░░╚██╔╝░░██║░░██╗██║░░██║  ╚════╝  ██║╚██╔╝██║██║░░██║
+███████╗╚██████╔╝██████╔╝░░░██║░░░╚█████╔╝╚█████╔╝  ░░░░░░  ██║░╚═╝░██║██████╔╝
+╚══════╝░╚═════╝░╚═════╝░░░░╚═╝░░░░╚════╝░░╚════╝░  ░░░░░░  ╚═╝░░░░░╚═╝╚═════╝░
+
+
+
+Project Name : BIGI
+Creator : @darkmakerofc , @mr-nima-x 
+
+*/
+
+module.exports = {
+  OWNER_NUMBER: "94768096838",
+  SUDO_NUMBERS : "94768096838",
+  OWNER_NAME: "『 BI GI 』",
+  BOT_NAME : "ＳＹＣＯ",
+  ZUSYCO_FILE : "ZUSYCO-QR",
+  LOGOS: {
+    ALIVE: 'https://raw.githubusercontent.com/GayaGihana/GayaGihana/main/STK-20240414-WA0022.webp',
+    WELCOME: 'https://raw.githubusercontent.com/GayaGihana/GayaGihana/main/STK-20240414-WA0022.webp',
+    GOODBYE: 'https://raw.githubusercontent.com/GayaGihana/GayaGihana/main/STK-20210811-WA0242.webp'
+  },
+  STOP_SEND : {
+        WELCOME_MESSAGE : false,
+        GOODBYE_MESSAGE : true,
+    },
+  DESABLE_WELCOME_GOODBYE_JIDS : [],
+  TIME_ZONE :  "Asia/Colombo",
+  WORK_TYPE : "public",
+  WORK_MODE : "onlygroups",
+  WORK_MODE_MESSAGE_SEND : false,
+  CAPTION : 'GIHAA ᴍᴅ BOT',
+  STICKER_PACK : '[ ZUSYCO ]',
+  BAD_WORDS : 'fuck,pussy',
+  ANTI_BAD : false,
+  ANTI_LINKS : 'chat.whatsapp.com',
+  ANTI_LINK : false,
+  ANTI_LINK_KICK : false,
+  MAX_SIZE : 100,
+  MOROCCO_NUMBERS : {
+        BLOCK : false,
+        NOT_WORKING : false,
+  },
+  READ : {
+     ALL_MESSAGES : false,
+     ONLY_COMMANDS : false,
+     STATUS : true,
+  },
+  BOT_OFFLINE : false,
+  INBOX_BLOCK : false,
+  AUTO_REACT : true,
+  OWNER_REACT : '👍',
+  AUTO_BGM : true,
+  BGM : {
+    "chaio" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-caio.mp3",
+    "leo" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-leo.mp3",
+    "tance" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-tance0.mp3",
+    "romant" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-floot.mp3",
+    "serio" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-serio.mp3",
+    "chill" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-wichil.mp3",
+    "yaka" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-yaka.mp3",
+    "animel" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-animel.mp3",
+    "bitch" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-bitch.mp3",
+    "papa" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-papa.mp3",
+    "shee" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-sheela.mp3",
+    "thugs" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-thugs.mp3",
+
+  },
+  AUTO_STICKER : true,
+  STICKERS : {
+    "pit" : "https://raw.githubusercontent.com/GayaGihana/GayaGihana/main/STK-20211211-WA0134.webp",
+    "agei" : "https://raw.githubusercontent.com/GayaGihana/GayaGihana/main/STK-20210812-WA0012.webp",
+    "ne" : "https://raw.githubusercontent.com/GayaGihana/GayaGihana/main/STK-20220128-WA0024.webp",
+    "ela" : "https://raw.githubusercontent.com/GayaGihana/GayaGihana/main/STK-20211211-WA0139.webp",
+    "oi" : "https://raw.githubusercontent.com/GayaGihana/GayaGihana/main/STK-20240119-WA0022.webp",
+    "hello" : "https://raw.githubusercontent.com/GayaGihana/GayaGihana/main/STK-20240420-WA0004.webp",
+    "ade" : "https://raw.githubusercontent.com/GayaGihana/GayaGihana/main/STK-20210918-WA0089.webp",
+    "bn" : "https://raw.githubusercontent.com/GayaGihana/GayaGihana/main/STK-20210918-WA0089.webp",
+    "💔" : "https://raw.githubusercontent.com/GayaGihana/GayaGihana/main/STK-20210918-WA0089.webp",
+  },
+  LOAD_ANIMATION : false,
+ 
+};
